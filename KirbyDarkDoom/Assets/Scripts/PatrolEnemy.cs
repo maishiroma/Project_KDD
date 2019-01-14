5fa638cfa69668c4a4000dcce1b93275e232361e
shiro105 <shiro105@mail.chapman.edu>
2019-01-14 15:33:07 -0800

    Added timed turning for enemy AI

--- KirbyDarkDoom/Assets/Scripts/PatrolEnemy.cs
+++ KirbyDarkDoom/Assets/Scripts/PatrolEnemy.cs
@@ -8,6 +8,10 @@ using UnityEngine;
 
 public class PatrolEnemy : BaseEnemy
 {
+    [Header("Sub Variables")]
+    public bool moveSetDisitance = false;
+    public float turnTime = 5f;
+
     // This enemy will constantly move
     public override void Move()
     {
@@ -21,18 +25,51 @@ public class PatrolEnemy : BaseEnemy
         }
     }
 
-    // Calls the move function that was overriden
-    private void FixedUpdate()
+    // Initializes the TurnAroundRepeater, which will make the enemy turn around automatically after X seconds
+	private void Start()
+	{
+        if(moveSetDisitance == true)
+        {
+            InvokeRepeating("TurnAround", turnTime, turnTime);
+        }
+	}
+
+    // This method is used to verify that the values in the inspector are valid
+	[ExecuteInEditMode]
+    private void OnValidate()
+    {
+        turnTime = Mathf.Clamp(turnTime, 1f, 100f);
+    }
+
+	// Calls the move function that was overriden
+	private void FixedUpdate()
 	{
         Move();
 	}
 
-    // If the enemy collides into a wall, they will change directions
+	// Behavior when the enemy hits something
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-        if(collision.gameObject.tag == "Wall" && collision.gameObject.layer == LayerMask.NameToLayer("Indestructable"))
+        if(collision.gameObject.tag == "Wall")
         {
-            isFacingRight = !isFacingRight;
+            // If the enemy collides into a wall, they will change directions
+            TurnAround();
+        }
+        else if(collision.gameObject.tag == "Enemy")
+        {
+            // If the enemy collides into another enemy, they will change directions
+            TurnAround();
+        }
+        else if(collision.gameObject.tag == "Player")
+        {
+            // TODO: Player takes damage
+            gameObject.SetActive(false);
         }
 	}
+
+    // Used in an Invoke to make the enemy turn around.
+    private void TurnAround()
+    {
+        isFacingRight = !isFacingRight;
+    }
 }
