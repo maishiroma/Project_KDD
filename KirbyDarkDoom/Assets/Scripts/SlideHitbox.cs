38e3bd323840a341e8a6514b773513159e351b5b
shiro105 <shiro105@mail.chapman.edu>
2019-01-20 15:02:55 -0800

    Added Sliding mechanic

---
+++ KirbyDarkDoom/Assets/Scripts/SlideHitbox.cs
@@ -0,0 +1,29 @@
+﻿/*  This detects whether the player runs into a specific object
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideHitbox : MonoBehaviour {
+
+	[Header("External Components")]
+    public PlayerController player;
+
+	// If the player runs into a specific entiry, 
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if(collision.gameObject.tag == "Enemy")
+        {
+            // Defeat the enemy and stop the slide
+            collision.gameObject.SetActive(false);
+            player.isSliding = false;
+            gameObject.SetActive(false);
+        }
+        else if(collision.gameObject.tag == "Block")
+        {
+            // Destroy the block and keep going
+            collision.gameObject.SetActive(false);
+        }
+	}
+}
